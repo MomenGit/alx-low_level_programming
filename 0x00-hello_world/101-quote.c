6300683e37009cedaf11d1330033e90b0248ee85
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-17 21:58:02 +0200

    Fix betty style 101-quote script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char *q = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
