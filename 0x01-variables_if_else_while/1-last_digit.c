83d8f2d7a228d51d7d080470068f990d0287efb7
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-18 00:22:48 +0200

    Write 1-last_digit.c script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -7,6 +9,19 @@
  */
 int main(void)
 {
+	int n,x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	x = n % 10;
+
+        if (x > 5)
+                printf("Last digit of %d is %d and is greate than 5\n", n, x);
+        else if (x == 0)
+                printf("Last digit of %d is %d and is 0\n", n, x);
+        else
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+
 	return (0);
 }
 
