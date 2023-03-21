29a320b7a1c707a5d4306c12f6847d40ec434b88
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-21 17:22:18 +0200

    Write 3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-    int j;
-    char i;
+	int j;
+	char i;
 
-    for (j = 0; j < 10; j++)
-    {
-        for (i = 'a'; i <= 'z'; i++)
-        {
-            _putchar(i);
-        }
-        _putchar('\n');
-    }
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
- * On error, .
+ * _islower - returns 1 if the input is a lower case
+ * otherwise returns 0
+ * @c: input character
+ *
+ * Return: On success 1.
+ * On error, 0.
  */
-void name(void)
+int _islower(int c)
 {
-
+	return (c >= 'a' && c <= 'z') ? 1 : 0;
 }
-
