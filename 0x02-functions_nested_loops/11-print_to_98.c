c1a44fd12cc590486e1472920c1259ba00b72497
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-21 23:47:53 +0200

    Write 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
+ * @n: input number
+ *
+ * Return: On success 0.
  * On error, .
  */
-void name(void)
+void print_to_98(int n)
 {
+	for (; n > 98; n--)
+	{
+		printf("%d, ", n);
+	}
 
+	for (; n < 98; n++)
+	{
+		printf("%d, ", n);
+	}
+
+	printf("%d\n", 98);
 }
-
