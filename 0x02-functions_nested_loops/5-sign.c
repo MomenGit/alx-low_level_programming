07dbd85394d92a38250d77be551818d88ea981ed
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-21 21:15:04 +0200

    Write 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
- * On error, .
+ * print_sign - prints 1 for +ve no., -1 for -ve no. and 0 for 0
+ * @n: input number
  */
-void name(void)
+int print_sign(int n)
 {
-
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
 }
-
