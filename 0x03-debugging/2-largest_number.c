9b7697c75680de32a0803fa9dc7752356aea60ed
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-22 16:44:35 +0200

    Fix 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
+	if (a > b)
 		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
 	else
-	{
+		largest = b;
+
+	if (c > largest)
 		largest = c;
-	}
 
 	return (largest);
 }
