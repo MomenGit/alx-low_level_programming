6abf6fd79fb079ad98809eb4ed1072e89f8021c3
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-23 17:13:26 +0200

    Write 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints nubmers from 0 to 9 except 2 and 4
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
  */
 void print_most_numbers(void)
 {
+	char c = '0';
 
+	while (c <= '9')
+		if (c != '2' && c != '4')
+			_putchar(c++);
+
+	_putchar('\n');
 }
-
