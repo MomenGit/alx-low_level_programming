ed357d35cb3360dba43b01799445d42cf3a0033e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-23 17:27:28 +0200

    Write 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,10 +3,17 @@
 /**
  * print_line - draws a straight line in the terminal using n of _
  * @n: integer number of _
- * 
+ *
  */
 void print_line(int n)
 {
+	int i;
 
+	for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+			_putchar('1');
+		_putchar('0' + i % 10);
+	}
+	_putchar('\n');
 }
-
