db3066160cf8a2c43042a994c2bd822d7908a937
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-24 23:03:07 +0200

    Write 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,26 @@
  */
 void print_number(int n)
 {
+	int i;
 
+	i = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+
+	while (n)
+	{
+		if (n > i)
+			i = i * 10;
+		else
+		{
+			i = i / 10;
+			_putchar('0' + n / i);
+			n = n % i;
+		}
+	}
 }
-
