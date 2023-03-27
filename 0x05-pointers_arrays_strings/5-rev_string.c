fbf1c70e6355dcb510b284eb7ef26ed8dba37180
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-27 22:25:58 +0200

    Write 4-print_rev.c and 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,20 @@
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
- * On error, .
+ * rev_string - reverse a string
+ * @s: string to be reversed
  */
-void name(void)
+void rev_string(char *s)
 {
+	int len, i;
+	char tmp;
 
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
 }
-
