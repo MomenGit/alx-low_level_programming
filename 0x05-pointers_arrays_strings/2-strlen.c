3714b94d56756491153962b1efa738613c2b9f2c
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-27 17:25:15 +0200

    Write 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,18 @@
 /**
  * name - description
  * @para: desc
- * 
+ *
  * Return: On success .
  * On error, .
  */
-void name(void)
+int _strlen(char *s)
 {
+	int len;
 
+	len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return len;
 }
-
