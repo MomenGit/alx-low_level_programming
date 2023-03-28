402143e238aeead70a66fc46b5f3814856bb2895
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-28 03:56:40 +0200

    Write 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
+
 /**
- * name - description
- * @para: desc
- * 
+ * print_array - prints n elements of an array of integers
+ * @:
+ *
  * Return: On success .
  * On error, .
  */
-void name(void)
+void print_array(int *a, int n)
 {
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", a[i], (i == n - 1) ? "\n" : ", ");
+	}
 }
-
