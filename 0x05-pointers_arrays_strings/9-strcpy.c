5a5f08fbf4726a58d533bbfbfacde43a81a38fd6
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-28 04:07:42 +0200

    Write 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,23 @@
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @src: the string to be copied
+ * @dest: the string to hold the copy
+ *
+ * Return: On success dest.
  * On error, .
  */
-void name(void)
+char *_strcpy(char *dest, char *src)
 {
+	int i;
 
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
-
