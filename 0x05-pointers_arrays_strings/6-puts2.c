16b842c5f5dc7e591931d34791e01c08ab2b1224
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-28 03:03:59 +0200

    Write 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,23 @@
 /**
- * name - description
- * @para: desc
- * 
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: string
+ *
  * Return: On success .
  * On error, .
  */
-void name(void)
+void puts2(char *str)
 {
+	int i, len;
 
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
-
