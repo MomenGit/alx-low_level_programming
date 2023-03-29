755005fc01320702e7050fd52ca656b005903294
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-29 13:35:18 +0200

    Write 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,6 +6,16 @@
  * Return: On success the concatenated string.
  * On error, .
  */
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
+	char *str = dest;
+
+	while (*str != '\0')
+		str++;
+
+	while (*src != '\0')
+		*str++ = *src++;
+
+	*str = '\0';
+	return (dest);
 }
