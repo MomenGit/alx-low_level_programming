d8b3abf344edb20c4ab343caf504d4218ad1ed43
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-29 16:48:33 +0200

    Write 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,24 @@
 /**
- * name - description
- * @para: desc
- * 
+ * leet - encodes a string into 1337
+ * @str:
+ *
  * Return: On success .
  * On error, .
  */
-void name(void)
+char *leet(char *str)
 {
+	int i, j;
 
+	char *encoders[] = {"aA4",
+						"eE3",
+						"oO0",
+						"tT7",
+						"lL1"};
+
+	for (i = 0; str[i] != '\0'; i++)
+		for (j = 0; j < 5; j++)
+			if (str[i] == encoders[j][0] || str[i] == encoders[j][1])
+				str[i] = encoders[j][2];
+
+	return (str);
 }
-
