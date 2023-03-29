b34cd568c9e45fa50133c8520ffea8df69d39947
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-29 15:38:29 +0200

    Write 7-leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,31 @@
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
+ * cap_string - capitalizes all words of a string.
+ * @str: string to operated on
+ *
+ * Return: On success str.
  * On error, .
  */
-void name(void)
+char *cap_string(char *str)
 {
+	int i, j;
 
+	char *separators = " ,.\n\t:!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'z') && str[i] != '?')
+		{
+			for (j = 0; separators[j] != '\0'; j++)
+			{
+				if (str[i] == separators[j])
+				{
+					if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+						str[i + 1] = (char)(str[i + 1] - 32);
+					break;
+				}
+			}
+		}
+	}
+
+	return (str);
 }
-
