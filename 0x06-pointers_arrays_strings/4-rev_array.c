dd3342998b9686df0da7292c217a5fad94946d97
momengit <51290928+MomenGit@users.noreply.github.com>
2023-03-29 14:53:33 +0200

    Write 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,18 @@
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
- * On error, .
+ * rev_array - reverses int array
+ * @a: array of integers
+ * @n: size of array a
  */
-void name(void)
+void reverse_array(int *a, int n)
 {
+	int i, tmp;
 
+	i = 0;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
 }
-
