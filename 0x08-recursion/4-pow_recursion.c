5d23f9321f4fe663f923f0d9af98cd49ee1d80d2
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-06 22:41:06 +0200

    Write 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,16 @@
 /**
- * name -
+ * _pow_recursion - calculates x to the power of y
  * @:
  *
- * Return: On success.
+ * Return: On success the value of x raised to the power of y.
  * On error, .
  */
-void name()
+int _pow_recursion(int x, int y)
 {
+	if (y > 0)
+		return x * _pow_recursion(x, --y);
+	if (y < 0)
+		return -1;
+
+	return 1;
 }
