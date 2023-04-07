979bf88c7422b53652fe909f6ece099fe606ebc5
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-07 08:08:24 +0200

    Write 2-strlen_recursion.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s:
+ *
+ * Return: On success.
+ * On error, .
+ */
+int _strlen_recursion(char *s)
+{
+}
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,21 @@
 /**
+ * _strlen_rec - returns the length of a string
+ * @s: string to be measured
+ * @i: length measured
+ *
+ * Return: On success length.
+ * On error, .
+ */
+int _strlen_rec(char *s, int i)
+{
+	if (*(s + i) == '\0')
+		return (i);
+	else
+		return (_strlen_rec(s, ++i));
+
+	return (0);
+}
+/**
  * _strlen_recursion - returns the length of a string
  * @s:
  *
@@ -7,4 +24,5 @@
  */
 int _strlen_recursion(char *s)
 {
+	return (_strlen_rec(s, 0));
 }
