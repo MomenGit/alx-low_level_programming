6611f89e03d0bfba77990f2b366872a8471b4f35
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-07 08:24:03 +0200

    Write 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,51 @@
 /**
- * _strlen_recursion - returns the length of a string
- * @s:
+ * _strlen_rec - returns the length of a string
+ * @s: string to be measured
+ * @i: length measured
  *
- * Return: On success.
+ * Return: On success length.
  * On error, .
  */
-int _strlen_recursion(char *s)
+int _strlen_rec(char *s, int i)
 {
+	if (*(s + i) == '\0')
+		return (i);
+	else
+		return (_strlen_rec(s, ++i));
+
+	return (0);
+}
+/**
+ * is_palindrome_logic - returns whether a string is palindrome or not
+ * @s: string to be checked
+ * @len: length of string s
+ * @i: index of the checked char of s
+ *
+ * Return: On success 1.
+ * On error, .
+ */
+int is_palindrome_logic(char *s, int len, int i)
+{
+	if (i == len / 2)
+		return (1);
+
+	if (*(s + i) == *(s + len - i - 1))
+		return (is_palindrome_logic(s, len, ++i));
+
+	return (0);
+}
+/**
+ * is_palindrome - returns whether a string is palindrome or not
+ * @s: string to be checked
+ *
+ * Return: On success 1.
+ * On error, .
+ */
+int is_palindrome(char *s)
+{
+	int length;
+
+	length = _strlen_rec(s, 0);
+
+	return (is_palindrome_logic(s, length, 0));
 }
