0cf1dfa35c4053cf7bcb97d1544dfc49fec5d01b
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-11 02:35:42 +0200

    Write 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * prints number of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
