c4db8cffb5cf41a2cea92686ba97eee6181051cd
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-11 02:39:42 +0200

    Write 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * prints program name
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+
+	return (0);
+}
