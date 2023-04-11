7ba275d84fc6eb4727fe784ce4450e90164e4052
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-12 01:21:01 +0200

    Write 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,32 @@
+#include <stdlib.h>
+
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
- * On error, .
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string to be duplicated
+ *
+ * Return: On success pointer to duplicated string.
+ * On error, NULL.
  */
-void name(void)
+char *_strdup(char *str)
 {
+	int i;
+	char *new_str;
 
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	new_str = malloc(i + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		new_str[i] = str[i];
+
+	new_str[i] = str[i];
+	return (new_str);
 }
-
