2f8f34fc1797cf6337ea4dfc9541053cf8c72e25
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-12 06:33:21 +0200

    Write 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,39 @@
+#include <stdlib.h>
+
 /**
  * name - description
  * @para: desc
- * 
+ *
  * Return: On success .
  * On error, .
  */
-void name(void)
+char *str_concat(char *s1, char *s2)
 {
+	int i, j;
+	char *concat_str;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	concat_str = malloc(i + j + 1);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		concat_str[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		concat_str[i + j] = s2[j];
+
+	concat_str[i + j] = '\0';
+
+	return (concat_str);
 }
-
