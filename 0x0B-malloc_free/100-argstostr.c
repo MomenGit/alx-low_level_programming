39da8f5ca90180c1ccda16f482fb5afde1eee609
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-12 17:52:24 +0200

    Write 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
- * On error, .
+ * argstostr - concatenates all the arguments of your program
+ * @ac: number of arguments
+ * @av: string arguments
+ *
+ * Return: On success pointer to concatenated string.
+ * On error, NULL.
  */
-void name(void)
+char *argstostr(int ac, char **av)
 {
+	char *str;
+	int size, i, j, k;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	size = 0;
+
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+
+	str = malloc(size + ac + 1);
+	str[size + ac] = '\0';
+
+	for (i = 0, k = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			str[k] = av[i][j];
+		str[k++] = '\n';
+	}
+
+	return (str);
 }
-
