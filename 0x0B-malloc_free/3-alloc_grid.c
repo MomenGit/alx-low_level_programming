42cfe243afca3222061d9ff5cdac4115b6c1bb30
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-12 13:31:40 +0200

    Write 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,35 @@
+#include <stdlib.h>
+
 /**
- * name - description
- * @para: desc
- * 
- * Return: On success .
+ * alloc_grid - creates a pointer to a 2 dimensional array of integers
+ * @width: int number width of grid
+ * @height: int number height of grid
+ *
+ * Return: On success pointer to created docss.
  * On error, .
  */
-void name(void)
+int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **grid;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+
+	return (grid);
 }
-
