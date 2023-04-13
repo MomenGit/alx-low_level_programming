9d60e0e0a07cdf9021c0faaa11f60893c45ff4df
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-13 17:19:16 +0200

    Write 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,15 @@
+#include <stdlib.h>
+
 /**
  * malloc_checked - allocates memory using malloc
- * @b: desc
+ * @b: number of bytes
  */
 void *malloc_checked(unsigned int b)
 {
+	void *pointer = malloc(b);
+
+	if (pointer == NULL)
+		exit(3);
+
+	return (pointer);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,21 @@
+#include <stdlib.h>
+
 /**
  * _calloc - allocates memory for an array, using malloc
- * @nmemb: desc
- * @size: desc
+ * @nmemb: number of array members
+ * @size: size of member
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	return (arr);
 }
