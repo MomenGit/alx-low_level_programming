f39c54bb250878cbd948dabbe85e5bf70f0c222e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-13 17:58:12 +0200

    Fix includes

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,9 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes
+ *
+ * Return: On success pointer to new allocated memory.
+ * On error, terminates.
  */
 void *malloc_checked(unsigned int b)
 {
