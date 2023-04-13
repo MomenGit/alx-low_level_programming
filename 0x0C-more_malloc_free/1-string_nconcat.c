cc86961e85205d5b9a6f79237eb564a25ef53b9b
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-13 18:25:22 +0200

    Write 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,35 @@
+#include <stdlib.h>
+
 /**
  * string_nconcat - concatenates two strings
- * @s1: desc
- * @s2: desc
- * @n: desc
+ * @s1: first string
+ * @s2: string to be concatenated to s1
+ * @n: number of characters of s2
  *
  * Return: On success .
  * On error, .
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, j, k, size;
+	char *str;
+
+	size = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+		size++;
+	for (j = 0; s2[j] != '\0'; j++)
+		size++;
+
+	str = malloc(size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (k = 0; *s1 != '\0'; k++)
+		str[k] = *s1++;
+	for (j = 0; *s2 != '\0' && j <= n; k++, j++)
+		str[k] = *s2++;
+	str[k] = '\0';
+
+	return (str);
 }
