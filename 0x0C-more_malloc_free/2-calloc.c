8f92ecb40aee08c72903a990529e1a013c1c4d7f
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-13 17:38:51 +0200

    Fix setting initial values to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - allocates memory for an array, using malloc
@@ -8,6 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	char *mod_arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -16,6 +18,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (arr == NULL)
 		return (NULL);
+
+	mod_arr = arr;
+	while (size)
+		mod_arr[size--] = 0;
 
 	return (arr);
 }
