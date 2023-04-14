04144ea8c5a2dc94f98ec0407d829221ef1ef430
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-14 14:27:00 +0200

    Fix malloc sizing

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * array_range - creates an array of integers
  * @min: desc
@@ -8,4 +10,20 @@
  */
 int *array_range(int min, int max)
 {
+	int *arr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = min + i;
+
+	return (arr);
 }
