993b8d240e3a1fe555249d14c50b7b48efd78e31
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-14 14:54:46 +0200

    Write 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: desc
@@ -6,4 +8,27 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *new_ptr;
+	char *ptrd;
+	unsigned int i, size;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (ptr != NULL)
+	{
+		size = (new_size > old_size) ? new_size : old_size;
+		ptrd = ptr;
+		for (i = 0; i < size; i++)
+			new_ptr[i] = ptrd[i];
+	}
+
+	return (new_ptr);
 }
