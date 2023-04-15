7342639208b0ad66aea091284c6b57d59b1a73cd
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-15 14:42:02 +0200

    Write 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * prints program name
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int coins, dividend, cents, i;
+	int coin_types[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+	coins = 0;
+	dividend = 0;
+
+	for (i = 0; i < 5 && cents; i++)
+	{
+		dividend = cents / coin_types[i];
+		cents -= dividend * coin_types[i];
+		coins += dividend;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
