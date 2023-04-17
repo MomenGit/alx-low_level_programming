2018f96a5adebf1ea82d6415c4d0e2c093f7ab53
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-17 17:12:31 +0200

    Write 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog: initialize dog struct instance
+ * @d: ...
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
