d2c537942dbe8128b52223f5df73de0647543f15
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-18 17:33:54 +0200

    Add header guards

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - ...
  * @name: ...
@@ -15,3 +17,5 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
