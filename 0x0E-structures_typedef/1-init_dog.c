4d6bbdbe8235b9d34ca4ca66ffe89c8f6c0a5751
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 21:22:26 +0200

    Fix dog.h and add null safety to 1-init_dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- * init_dog: initialize dog struct instance
+ * init_dog - initialize dog struct instance
  * @d: ...
  * @name: ...
  * @age: ...
@@ -9,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
