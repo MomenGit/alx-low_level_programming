b4e9f13d78d02e9698ef86569062ae1b56b06ba7
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 21:31:41 +0200

    Fix missing space

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,5 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		d->owner = nil;
 
-	if (d->age)
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	else
-		printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, nil, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
