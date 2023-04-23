928e8a5396ac835ba6f9f20721355154bd494244
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 21:26:51 +0200

    Add more null safety

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,6 +17,8 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		d->owner = nil;
 
-	if (d != NULL)
+	if (d->age)
 		printf("Name: %s\nAge: %f\nOwner:%s\n", d->name, d->age, d->owner);
+	else
+		printf("Name: %s\nAge: %s\nOwner:%s\n", d->name, nil, d->owner);
 }
