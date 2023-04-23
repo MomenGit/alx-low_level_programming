c3d73e980966d83163974782691256fe51329b99
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 15:47:23 +0200

    Write 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,22 @@
+#include "variadic_functions.h"
+
 /**
  * print_numbers - prints numbers, followed by a new line.
- * @separator: the separator character
- * @n: number of variadic functions
+ * @separator: the string to be printed between numbers
+ * @n: number of variadic integers
  *
  * Return: Nothing.
  * On error, .
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list ptr;
+	va_start(ptr, n);
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ptr, int), (separator != NULL) ? separator : "");
+
+	printf("%d\n", va_arg(ptr, int));
+	va_end(ptr);
 }
