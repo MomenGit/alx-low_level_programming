0c2ada2ad96c0b37384f746d99ee2160dbd6bbdb
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 17:27:29 +0200

    Write 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,10 +1,63 @@
+#include "variadic_functions.h"
+
+/**
+ * separate - prints separator
+ *
+ * @format: a list of types of arguments passed to the function
+ * @i: index
+ */
+void separate(const char *const format, int i)
+{
+	if (format[i + 1] != '\0')
+		printf(", ");
+	else
+		printf("\n");
+}
+
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function
  *
  * Return: Nothing.
- * On error, .
  */
 void print_all(const char *const format, ...)
 {
+	int i, j, found;
+	const char *formats = "cifs";
+	va_list list_ptr;
+
+	va_start(list_ptr, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		j = 0, found = 0;
+		while (formats[j] != '\0')
+			if (format[i] == format[j++])
+			{
+				found = 1;
+				break;
+			}
+		if (found == 0)
+			continue;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(list_ptr, int));
+			separate(format, i);
+			break;
+		case 'i':
+			printf("%d", va_arg(list_ptr, int));
+			separate(format, i);
+			break;
+		case 'f':
+			printf("%f", va_arg(list_ptr, double));
+			separate(format, i);
+			break;
+		case 's':
+			printf("%s", va_arg(list_ptr, char *));
+			separate(format, i);
+			break;
+		}
+		i++;
+	}
 }
