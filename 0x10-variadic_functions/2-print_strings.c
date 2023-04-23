f4f5e9671f85563792e647a1a9aafe456ca87d8e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 16:44:06 +0200

    Write 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,11 +1,30 @@
+#include "variadic_functions.h"
+
 /**
  * print_strings - prints strings, followed by a new line.
- * @separator: the separator character
- * @n: number of variadic parameters
+ * @separator: the string to be printed between the strings
+ * @n: number of variadic strings
  *
  * Return: Nothing.
  * On error, .
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list list_ptr;
+	unsigned int i;
+	char *word;
+
+	if (n == 0)
+		return (0);
+
+	va_start(list_ptr, n);
+
+	for (i = 0; i < n - 1; i++)
+	{
+		word = va_arg(list_ptr, char *);
+		printf("%s", (word != NULL) ? word : "(nil)");
+		printf("%s", (separator != NULL) ? separator : "");
+	}
+	word = va_arg(list_ptr, char *);
+	printf("%s\n", (word != NULL) ? word : "(nil)");
 }
