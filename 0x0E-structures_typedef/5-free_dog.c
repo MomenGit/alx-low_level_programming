1cf84cf38b7003a8620781e0fa76240d09f00246
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-23 21:49:54 +0200

    Write 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,13 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ *
+ * @d: pointer to dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
