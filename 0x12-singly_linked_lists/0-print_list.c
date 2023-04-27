90238115c72039dfd406be9c22b0cf2319db03aa
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-27 18:06:20 +0200

    Write 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,24 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list
  *
- * @h: pointer to a list
+ * @h: the head of a list
  *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", i, (h->str != NULL) ? h->str : "(nil)");
+		i++;
+		h = h->next;
+	}
+
+	return (i);
 }
