970d859d6b6e47a3b5dc8233f59a1370403224cf
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-27 19:51:31 +0200

    Write 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -11,4 +12,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	int i;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL || str == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+		return (NULL);
+
+	for (i = 0; new->str[i] != '\0'; i++)
+		;
+
+	new->len = i;
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
 }
