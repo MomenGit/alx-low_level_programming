be0bcd9a80516a13e88cdff60fca2835e30abee9
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-27 20:15:57 +0200

    Write 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,4 +11,33 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node, *tmp;
+	unsigned int i;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+		return (NULL);
+
+	for (i = 0; new_node->str[i] != '\0'; i++)
+		;
+
+	new_node->next = NULL;
+	new_node->len = i;
+
+	if (*head != NULL)
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
 }
