00ef4c9ea61b221b152b5c773b460856b15592d6
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-27 17:45:15 +0200

    Prepare project files

---
+++ 0x12-singly_linked_lists/1-list-len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * list_len - number of elements in a linked list_t list
+ *
+ * @h: pointer to a list
+ *
+ * Return: length of the list
+ */
+size_t list_len(const list_t *h)
+{
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ *
+ * @head: the head of the list
+ * @str: the new added string
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+}
