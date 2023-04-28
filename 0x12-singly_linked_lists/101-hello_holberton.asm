05d1c94d49498b9b9d15ab0b449527e88cb45d3e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-28 15:11:53 +0300

    Fix code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,31 +1,21 @@
 ; File: hello_holberton.asm
-; Description: 64-bit assembly program that prints "Hello, Holberton" followed by a new line.
-
-extern printf
-
-section .text
-    global main
-
-main:
-    ; Prepare the stack for the call to printf
-    push rbp
-
-    ; Set up the arguments for printf
-    mov rdi, fmt     ; Format string
-    mov rsi, msg     ; Message string
-    mov rax, 0       ; Clear rax (varargs)
-
-    ; Call printf
-    call printf
-
-    ; Clean up the stack
-    pop rbp
-
-    ; Exit the program
-    mov rax, 60      ; System call for exit
-    xor rdi, rdi     ; Exit code 0
-    syscall
 
 section .data
-    msg: db "Hello, Holberton", 0
-    fmt: db "%s", 10, 0
+format db "%s", 10, 0
+message db "Hello, Holberton", 0
+
+section .text
+extern printf ;C function to be called
+global main ;main function
+
+main:
+ push rbp
+ mov rdi, format
+ mov rsi, message
+ mov rax, 0
+ call printf ;call C function.
+
+ pop rbp ;restore stack
+
+ mov rax, 0 ;normal, no error, return value
+ ret ;return
