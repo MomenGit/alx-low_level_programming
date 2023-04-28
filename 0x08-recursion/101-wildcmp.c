e828faae1143b64f75f801e38ca56c9d96c09aaf
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 00:01:19 +0300

    Write 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,31 @@
 /**
- * _strlen_recursion - returns the length of a string
- * @s:
+ * wildcmp - returns the comparison between two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: On success.
+ * Return: 1 if strings are identical otherwise 0.
  * On error, .
  */
-int _strlen_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
