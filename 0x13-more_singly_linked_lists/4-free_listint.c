90a6b2c453283760ab5dda24e460d2b5b49a547b
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 16:31:44 +0300

    Write 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,6 +5,15 @@
  *
  * @head: ...
  */
-void free_listint2(listint_t *head)
+void free_listint(listint_t *head)
 {
+	listint_t *tmp;
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		head = head->next;
+		free(tmp);
+		tmp = head;
+	}
 }
