906bfaa7099286dff683bdeef1d65e28e1ee554a
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 23:17:05 +0300

    Write 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,14 +1,50 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - ...
+ * insert_nodeint_at_index - inserts a new node at a given position.
  *
- * @head: ...
- * @idx: ...
- * @n: ...
+ * @head: the head of listint_t
+ * @idx: the index to be inserted at
+ * @n: the data of the new node
  *
  * Return: ...
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node, *tmp;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	new_node->next = NULL;
+
+	tmp = *head;
+	for (i = 0; tmp != NULL; i++)
+	{
+		tmp = tmp->next;
+		if (i == idx - 1)
+		{
+			if (tmp->next != NULL)
+				new_node->next = tmp->next;
+			tmp->next = new_node;
+			return (new_node);
+		}
+	}
+	return (NULL);
 }
