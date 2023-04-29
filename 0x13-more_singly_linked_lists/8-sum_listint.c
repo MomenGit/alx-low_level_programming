cfca0300ab228df0a0392047a084082c48ad0ed3
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 19:28:31 +0300

    Write 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,4 +9,17 @@
  */
 int sum_listint(listint_t *head)
 {
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
 }
