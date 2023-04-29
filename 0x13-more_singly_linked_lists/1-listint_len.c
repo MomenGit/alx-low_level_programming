0402a42b146721884a245e08826f7bc22d3f4f25
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 16:22:57 +0300

    Write 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - ...
+ * listint_len - counts the number of elements in a linked list
  *
  * @h: ...
- * Return: size_t
+ * Return: the number of elements in a listint_t
  */
 size_t listint_len(const listint_t *h)
 {
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
 }
