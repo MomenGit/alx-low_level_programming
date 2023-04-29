96a88e0b074bd4827ad1b93c44e16a6bbf9639cc
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 18:44:37 +0300

    Write 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,27 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - ...
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
  *
- * @head: ...
- * @index: ...
+ * @head: the head of the list
+ * @index: the index of the node starting at 0
  *
- * Return: listint_t
+ * Return: the nth node of a listint_t linked list
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (head != NULL)
+			head = head->next;
+		else
+			break;
+	}
+
+	if (i == index)
+		return (head);
+	else
+		return (NULL);
 }
