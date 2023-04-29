197d18b7b87c17197c2301502118fea561c5c032
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 23:28:37 +0300

    Write 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,4 +10,31 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *current, *deleted_node;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		deleted_node = *head;
+		*head = (*head)->next;
+		free(deleted_node);
+		return (1);
+	}
+
+	current = *head;
+	for (i = 0; current != NULL; i++)
+	{
+		if (i == index - 1)
+		{
+			deleted_node = current->next;
+			current->next = deleted_node->next;
+			free(deleted_node);
+			return (1);
+		}
+		current = current->next;
+	}
+	return (-1);
 }
