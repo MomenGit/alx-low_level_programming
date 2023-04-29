d98472c16d8d713ccd07ed595fe640e840bc805b
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 16:29:05 +0300

    Write 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,4 +10,28 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new_node, *tmp;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	new_node->n = n;
+
+	if (*head != NULL)
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (*head);
 }
