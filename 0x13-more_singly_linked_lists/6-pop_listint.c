14c08e14e266be759554b8bfcacb7135cf75c59e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-29 18:04:52 +0300

    Write 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,8 +5,18 @@
  *
  * @head: ...
  *
- * Return: ...
+ * Return: the data (n) in head
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *tmp;
+	int n;
+
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (n);
 }
