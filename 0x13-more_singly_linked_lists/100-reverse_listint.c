2ade8063cc60f5cc5b206854211013dde5dec177
momengit <51290928+MomenGit@users.noreply.github.com>
2023-04-30 18:04:20 +0300

    Write 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,4 +9,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *tmp;
+
+	tmp = *head;
+	*head = (*head)->next;
+	if (tmp->next->next != NULL)
+	{
+		reverse_listint(head);
+	}
+
+	tmp->next->next = tmp;
+	tmp->next = NULL;
+
+	return (*head);
 }
