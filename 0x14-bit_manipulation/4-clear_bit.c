67fb492c5936a43d5092fbdbdc8b20f381dc0121
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-04 22:50:20 +0300

    Write 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * get_bit - ...
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: integer number to be operated on
  * @index: the index, starting from 0 of the bit you want to get
  *
- * Return: ...
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
+
+	*n = *n & (~(1 << index));
+	return (1);
 }
