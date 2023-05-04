57df4da941fbfc86f42755d61e98345ff03746fe
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-04 22:17:07 +0300

    Write 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,30 @@
 #include "main.h"
 
 /**
- * print_binary - ...
+ * print_rec - prints the binary representation of a number
+ * @n: integer number to operated on
+ *
+ * Return: Nothing.
+ */
+void print_rec(unsigned long int n)
+{
+	if (n)
+	{
+		print_rec(n >> 1);
+		_putchar('0' + (n & 1));
+	}
+}
+
+/**
+ * print_binary - prints the binary representation of a number
  * @n: integer number to operated on
  *
  * Return: Nothing.
  */
 void print_binary(unsigned long int n)
 {
+	if (n == 0)
+		_putchar('0');
+	else
+		print_rec(n);
 }
