f2818237b4170a4390c997fe44303ff9dd86fe6b
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-04 22:24:10 +0300

    Write 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * get_bit - ...
+ * get_bit - returns the value of a bit at a given index.
  * @n: integer number to be operated on
  * @index: the index, starting from 0 of the bit you want to get
  *
- * Return: ...
+ * Return: the value of the bit at index index or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	n = n >> index;
+	return (n & 1);
 }
