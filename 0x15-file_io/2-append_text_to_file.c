653b130c36084c35371d33af459adf4733fcb5a0
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-12 19:08:18 +0300

    Write 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	return (0);
+	int fd;
+	size_t len, _write;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	_write = write(fd, text_content, len);
+	if (_write != len)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
