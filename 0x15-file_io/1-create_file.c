6f8d4d3d2b111747ed015b8ac2d7fe5e04b0156d
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-12 18:39:02 +0300

    Write 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,5 +9,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	return (0);
+	int fd;
+	size_t len, _write;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		_write = write(fd, text_content, len);
+		if (_write != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
