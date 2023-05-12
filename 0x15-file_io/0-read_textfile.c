abd7db98dc5bddc4d96bb1a066421ae7b07d9f24
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-12 15:55:48 +0300

    Write 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,5 +9,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	return (0);
+	int fd;
+	ssize_t _read, written;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	buf = malloc(letters);
+	if (buf == NULL || fd < 0)
+		return (0);
+
+	_read = read(fd, buf, letters);
+	if (_read < 0)
+		return (0);
+	written = write(STDOUT_FILENO, buf, letters);
+	if (written < 0)
+		return (0);
+	close(fd);
+
+	return ((_read < written) ? _read : written);
 }
