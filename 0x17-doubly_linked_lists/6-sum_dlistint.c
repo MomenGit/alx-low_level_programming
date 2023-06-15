704e0a248f3bf51dacef9e581f7afa6f09aed7bc
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 18:13:23 +0300

    Write 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint
+ *
+ * @head: head of a dlistint
+ * Return: sum of data of dlistint
+ */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	while (head->prev != NULL)
+		head = head->prev;
+
+	sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
 }
