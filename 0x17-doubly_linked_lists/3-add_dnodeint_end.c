bd718450bed6256101c08a7ad5a120e9b8e38ce8
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 15:30:54 +0300

    Write 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,34 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end -
+ *
+ * @head: node of dlistint
+ * @n: new value
+ * Return: dlistint_t* new node
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head != NULL)
+	{
+		while ((*head)->next != NULL)
+			*head = (*head)->next;
+		new_node->prev = *head;
+		(*head)->next = new_node;
+	}
+	else
+	{
+		new_node->prev = NULL;
+	}
+	*head = new_node;
+	return (*head);
 }
