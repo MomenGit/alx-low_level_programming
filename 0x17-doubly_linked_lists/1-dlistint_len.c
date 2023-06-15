4aef69e21958768b4d6e7fe7ace18c83bc53edf6
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 15:01:56 +0300

    Write 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,4 +2,21 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t len;
+
+	if (h == NULL)
+		return (0);
+
+	len = 0;
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
 }
