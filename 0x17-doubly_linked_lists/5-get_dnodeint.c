60b9bee485bb682d59919bb346b810d0ba8d5fdb
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 18:09:05 +0300

    Write 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Get the dnodeint at index object
+ *
+ * @head: head of dlistint
+ * @index: index of node to be returned
+ * Return: dlistint_t* node at index
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	size_t i;
+
+	if (head == NULL)
+		return (NULL);
+	while (head->prev != NULL)
+		head = head->prev;
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
+
+	return (head);
 }
