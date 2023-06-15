745d4d5221790a4b2b538fde87f4fc416c62a6b5
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 14:50:52 +0300

    Write 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a dlistint
+ *
+ * @h: head of dlistint
+ * Return: size_t length of list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t len;
+
+	if (h == NULL)
+		return (0);
+
+	len = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+
+	return (len);
 }
