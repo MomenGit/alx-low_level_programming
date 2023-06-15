b2f003255dcc037b8c4df52060e11da5149d1937
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 15:04:38 +0300

    Fix betty styling add function doc

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - the number of elements in a linked dlistint_t list
+ *
+ * @h: a list node
+ * Return: size_t length of dlist_int
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len;
