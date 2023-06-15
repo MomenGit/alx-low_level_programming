ad9acacc2c7f5c9876ee42affd7f05cf916d975a
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-15 18:01:03 +0300

    Write 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: node of dlistint
+ */
 void free_dlistint(dlistint_t *head)
 {
+	if (head == NULL)
+		return;
+	while (head->prev != NULL)
+		head = head->prev;
+
+	while (head->next != NULL)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
 }
