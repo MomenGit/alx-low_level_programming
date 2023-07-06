5efa5e3733e3bff19fa22caf8e8ce2dbe3cbcd7c
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-06 19:04:54 +0300

    Write 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,13 +1,22 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - ...
+ * hash_table_get - Retrieves a value associated with a key
  *
- * @ht: ...
- * @key: ...
- * Return: char*
+ * @ht: Pointer to a hash table
+ * @key: Key of the hash table node pair
+ * Return: The value associated with the element,
+ * or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	return (NULL);
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	node = ht->array[key_index((const unsigned char *)key, ht->size)];
+	if (node == NULL)
+		return (NULL);
+	return (node->value);
 }
