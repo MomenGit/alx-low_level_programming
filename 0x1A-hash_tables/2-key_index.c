396054491df1897c3e3422771676ebb2d422de12
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-06 18:39:37 +0300

    Performance opt and better doc

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,17 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - ...
+ * key_index - calculates the index of hash table node from its key
  *
- * @key: ...
- * @size: ...
- * Return: unsigned long int
+ * @key: the key of a hash node
+ * @size: the size of the hash table
+ * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
-
-	index = hash_djb2(key) % size;
-
-	return (index);
+	return (hash_djb2(key) % size);
 }
