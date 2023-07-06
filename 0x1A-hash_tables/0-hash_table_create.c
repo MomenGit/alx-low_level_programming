219acdf58880160e7f9795f0e78305df7f4a0738
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-06 18:36:12 +0300

    Write 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - ...
+ * hash_table_create - create a new hash table
  *
- * @size: ...
- * Return: hash_table_t*
+ * @size: the size of the new created table
+ * Return: pointer to new created table, or NULL On Error
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	return (NULL);
+	size_t i;
+	hash_table_t *tb;
+
+	tb = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (tb == NULL)
+		return (NULL);
+
+	tb->size = size;
+	tb->array = (hash_node_t **)calloc(tb->size, sizeof(hash_node_t *));
+	if (tb->array == NULL)
+	{
+		free(tb);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		tb->array[i] = NULL;
+
+	return (tb);
 }
