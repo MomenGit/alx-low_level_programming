601b42fcf2a16a6dc55b394abdbe7f20ed937477
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-06 18:36:29 +0300

    Write 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,5 +9,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (0);
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
 }
