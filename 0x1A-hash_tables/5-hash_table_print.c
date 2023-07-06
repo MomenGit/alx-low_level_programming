4b3e4e6293b6c24a725ab9cefb67a6fbaf332d6d
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-06 19:39:37 +0300

    Write 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,10 +1,32 @@
 #include "hash_tables.h"
+#include <stdio.h>
 
 /**
- * hash_table_print - ...
+ * hash_table_print - Print hash table in the form of Key: Value pair
  *
- * @ht: ...
+ * @ht: Pointer to a hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *bucket;
+	unsigned long int i, j;
+
+	if (ht == NULL)
+		return;
+	bucket = *ht->array;
+
+	printf("{");
+	for (i = 0, j = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while (bucket != NULL)
+		{
+			j++;
+			printf("'%s': '%s', ", bucket->key, bucket->value);
+			bucket = bucket->next;
+		}
+	}
+	if (j > 0)
+		printf("\b\b");
+	printf("}\n");
 }
