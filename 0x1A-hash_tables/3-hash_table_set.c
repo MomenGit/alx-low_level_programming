766d56e960968f9a2f0e3d140a5dbd1dfec908b9
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-07 21:03:35 +0300

    Fix uninitialized variable bug

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,12 +15,24 @@ hash_node_t *create_hash_node(const char *key, const char *value)
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->key = (char *)malloc(sizeof(char) * (strlen(key) + 1));
-	new_node->value = (char *)malloc(sizeof(char) * (strlen(value) + 1));
-	strcpy(new_node->key, key);
-	strcpy(new_node->value, value);
-	if (new_node->value == NULL || new_node->key == NULL)
+	if (new_node->key == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
+	strcpy(new_node->key, key);
+
+	new_node->value = (char *)malloc(sizeof(char) * (strlen(value) + 1));
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	strcpy(new_node->value, value);
+	new_node->next = NULL;
 
 	return (new_node);
 }
@@ -38,16 +50,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current_node;
 
-	if (key == NULL || !strcmp(key, "") || ht == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
+	if (strcmp(key, "") == 0)
+		return (0);
+
+	current_node = NULL;
 
 	index = key_index((unsigned char *)key, ht->size);
 	current_node = ht->array[index];
 	while (current_node != NULL)
 	{
-		if (!strcmp(current_node->key, key))
+		if (strcmp(current_node->key, key) == 0)
 		{
-			free(current_node->value);
+			if (current_node->value != NULL)
+				free(current_node->value);
 			current_node->value = malloc(strlen(value) + 1);
 			if (current_node->value == NULL)
 				return (0);
