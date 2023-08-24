c2309a7ac84fe36a15f165a5c38c5c51638b6935
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-08-24 16:20:16 +0300

    Write 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""Defines island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+
+    Args:
+        grid (list): a list of list of integers
+            - 0 represents a water zone
+            - 1 represents a land zone
+            - One cell is a square with side length 1
+            - cells are connected horizontally/vertically(not diagonally)
+            - Grid is rectangular, width and height don't exceed 100
+    """
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+
+    for i in range(1, height):
+        for j in range(1, width):
+            if grid[i][j] == 1:
+                if grid[i][j-1] == 0:
+                    perimeter += 1
+                if grid[i][j+1] == 0:
+                    perimeter += 1
+                if grid[i-1][j] == 0:
+                    perimeter += 1
+                if grid[i+1][j] == 0:
+                    perimeter += 1
+
+    return perimeter
