ab325a55907643b34380adfae6d750b9fb8c6677
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-08-24 16:34:32 +0300

    Fix starting point of iterations

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,30 +4,40 @@
 
 def island_perimeter(grid):
     """
-    Returns the perimeter of the island described in grid
+    Calculates the perimeter of the island described in grid
 
     Args:
-        grid (list): a list of list of integers
-            - 0 represents a water zone
-            - 1 represents a land zone
-            - One cell is a square with side length 1
-            - cells are connected horizontally/vertically(not diagonally)
-            - Grid is rectangular, width and height don't exceed 100
-    """
-    height = len(grid)
-    width = len(grid[0])
-    perimeter = 0
+        grid (list[list[int]]): a list of list of integers represents the grid
 
-    for i in range(1, height):
-        for j in range(1, width):
-            if grid[i][j] == 1:
-                if grid[i][j-1] == 0:
-                    perimeter += 1
-                if grid[i][j+1] == 0:
-                    perimeter += 1
-                if grid[i-1][j] == 0:
-                    perimeter += 1
-                if grid[i+1][j] == 0:
-                    perimeter += 1
+    Returns:
+        int: The perimeter of the island.
+
+    Constraints:
+        - 0 represents a water zone.
+        - 1 represents a land zone.
+        - One cell is a square with side length 1.
+        - Grid cells are connected horizontally/vertically (not diagonally).
+        - Grid is rectangular, with width and height not exceeding 100.
+        - Grid is completely surrounded by water,
+          and there is one island (or nothing).
+        - The island doesn't have “lakes” (water inside that isn't connected to
+          the water around the island).
+    """
+
+    if not grid:
+        return 0
+
+    perimeter = 0
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                for dir_r, dir_c in directions:
+                    try:
+                        if grid[row+dir_r][col+dir_c] == 0:
+                            perimeter += 1
+                    except Exception as exp:
+                        continue
 
     return perimeter
